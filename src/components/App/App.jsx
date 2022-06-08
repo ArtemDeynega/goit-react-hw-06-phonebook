6cbd69a00d532737feaacc40e968662391e1ea44
artem <73790146+ArtemDeynega@users.noreply.github.com>
2022-06-08 18:13:54 +0300

    getFilterContacts done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,12 +3,8 @@ import { GlobalStyles } from 'Styles/GlobalStyles/GlobalStyles';
 import { ContactEditor } from 'components/ContactEditor';
 import { SectionTitle } from 'components/Title';
 import { ContactList } from 'components/ContactList';
-import { getContact } from 'redux/contacts';
-import { useSelector } from 'react-redux';
 
 export const App = () => {
-  const contacts = useSelector(getContact);
-  console.log(contacts);
   return (
     <>
       <SectionTitle title="Phonebook">
